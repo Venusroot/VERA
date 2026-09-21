91e78aaecc7ce7f2671d5879b2eb02a3f9b7c61b
Augusto Mota <augusto.mt.dev@gmail.com>
2026-09-21 23:33:27 +0000

    Criando e configurando Server pelo Codespace

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+$host = 'db'; // Nome do serviço no docker-compose
+$db   = 'vera';
+$user = 'admin';
+$pass = 'admin,3';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     // Conexão bem-sucedida!
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
